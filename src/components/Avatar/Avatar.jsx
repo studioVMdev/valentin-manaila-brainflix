72249c8fd3abee9f61847882eda8621e943d4581
Valentin Manaila <valentin.manaila@gmail.com>
2022-02-25 05:33:20 +0000

    initial commit

--- src/components/Avatar/Avatar.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Avatar.scss";
-
-const Avatar = ({ src, className }) => {
-	return <img className={className} src={src} alt="userimage" />;
-};
-
-export default Avatar;
