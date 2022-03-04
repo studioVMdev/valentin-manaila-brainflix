8f9c10cc19c0a6cc11d09cac88ac9cb081c94881
Valentin Manaila <valentin.manaila@gmail.com>
2022-03-04 18:31:54 +0000

    complete static video player controls

--- src/components/VideoPlayer/VideoPlayer.jsx
+++ src/components/VideoPlayer/VideoPlayer.jsx
@@ -11,10 +11,33 @@ const VideoPlayer = ({ videoObj }) => {
 					type="video/mp4"
 					src={videoObj.video}
 					className="video-player__player"
-					controls
+					// controls
 				></video>
-				<div className="video-player__controls"></div>
+				<div className="player">
+					<div className="player__left-wrapper player-wrapper">
+						<div className="player__play-button"></div>
+					</div>
+
+					<div className="player__scrubbing player-wrapper">
+						<div className="timeline">
+							<div className="timeline__scrubbing-icon"></div>
+							<div className="timeline__duration"></div>
+							<div className="timeline__buffering"></div>
+							<div className="timeline__current"></div>
+						</div>
+						<div className="time">
+							<div className="time__current-time">1:20/</div>
+							<div className="time__time-remaining"> 4:01</div>
+						</div>
+					</div>
+
+					<div className="player__right-wrapper player-wrapper">
+						<div className="player__fullscreen-button"></div>
+						<div className="player__volume-button"></div>
+					</div>
+				</div>
 			</div>
+			{/* <div className="video-player__controls"></div> */}
 		</section>
 	);
 };
