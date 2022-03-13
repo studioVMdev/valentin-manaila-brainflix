636ad81b19698b512cca5abda601889b76dd5a36
Valentin Manaila <valentin.manaila@gmail.com>
2022-03-13 09:46:51 +0000

    submission commit

--- src/components/VideoItem/VideoItem.jsx
+++ src/components/VideoItem/VideoItem.jsx
@@ -4,7 +4,12 @@ import { Link } from "react-router-dom";
 const VideoItem = ({ videoDetailsObj }) => {
 	const { id, image, title, channel } = videoDetailsObj;
 	return (
-		<Link to={`/video/${id}`}>
+		<Link
+			to={`/video/${id}`}
+			onClick={() => {
+				window.scrollTo(0, 0);
+			}}
+		>
 			<div id={id} className="video-item">
 				<img src={image} alt={title} className="video-item__thumbnail" />
 				<div className="video-item__info-wrapper">
